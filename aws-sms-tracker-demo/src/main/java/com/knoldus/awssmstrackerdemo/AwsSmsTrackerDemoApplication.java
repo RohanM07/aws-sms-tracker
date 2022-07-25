0bec77b855d7e760f66d2e7fe5c952915518dc0a
RohanM07 <rohan.mutyal@knoldus.com>
2022-07-25 18:37:27 +0530

    Initial commit for aws-sms-tracker-poc

---
+++ aws-sms-tracker-demo/src/main/java/com/knoldus/awssmstrackerdemo/AwsSmsTrackerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.knoldus.awssmstrackerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSmsTrackerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSmsTrackerDemoApplication.class, args);
+	}
+
+}
---
+++ aws-sms-tracker-demo/src/test/java/com/knoldus/awssmstrackerdemo/AwsSmsTrackerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoldus.awssmstrackerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSmsTrackerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
